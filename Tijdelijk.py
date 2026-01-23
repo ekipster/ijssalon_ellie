6d170b754cb0d8135257be67a382027783445879
edwin <edwinkip@hotmail.com>
2026-01-23 20:06:42 +0100

    functies toegevoegd

--- Tijdelijk.py
+++ Tijdelijk.py
@@ -1,17 +1,23 @@
-Mijn_dictionary = {"aardbei" : 3,
-                   "Vanille" : 4,
-                   "chocolade" : 5}
-Aanbieding = Mijn_dictionary["aardbei"] * 0.8
-Reclame_tekst = (f"vandaag in de aanbieding: Vanille-ijs, 1 liter - slechts €{Aanbieding}")
-Reclame_tekst2 = Reclame_tekst[:62]
-Reclame_tekst3 = Reclame_tekst2.upper()
-Reclame_tekst4 = Reclame_tekst3.split()
-el = Reclame_tekst4
-for el in Reclame_tekst4:
-    if len(el) > 4:
-        print(el.upper())
-    else:
-        print(el.lower())
+from helper import decoreer
+
+def print_aanbieding():
+    Mijn_dictionary = {"aardbei" : 3,
+                    "Vanille" : 4,
+                    "chocolade" : 5}
+    Aanbieding = Mijn_dictionary["aardbei"] * 0.8
+    Reclame_tekst = (f"vandaag in de aanbieding: Vanille-ijs, 1 liter - slechts €{Aanbieding}")
+    Reclame_tekst2 = Reclame_tekst[:62]
+    Reclame_tekst3 = Reclame_tekst2.upper()
+    Reclame_tekst4 = Reclame_tekst3.split()
+    el = Reclame_tekst4
+    for el in Reclame_tekst4:
+        if len(el) > 4:
+            print(el.upper())
+        else:
+            print(el.lower())
+
+decoreer("aanbieding")
+print_aanbieding()
         
     
 
